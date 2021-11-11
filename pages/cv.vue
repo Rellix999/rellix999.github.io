2eb501ef4f4ca4ce7aecf2c9135d98d95437de51
Rellix999 <rellix_@hotmail.com>
2021-11-11 13:35:42 +0100

    Initial commit for the CV page

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,6 +1,118 @@
 <template>
   <div class="flex flex-col">
     <PageHeading title="CV" subtitle="The course of my life: experience, skills and qualifications." />
-    <section>CV</section>
+    <section class="py-4">
+      <div class="container mx-auto px-4 md:px-0">
+        <section class="flex flex-col border-b-2 lg:flex-row lg:border-b-0 mb-3 pb-3">
+          <div class="flex w-full lg:w-1/4 mb-2">
+            <div class="w-full h-full lg:border-r-2 mr-4">
+              <div class="font-semibold text-2xl text-gray-700 uppercase">Personalia</div>
+            </div>
+          </div>
+          <div class="w-full lg:w-2/4">
+            <div class="flex flex-wrap">
+              <div class="w-1/3">Name:</div>
+              <div class="w-2/3">Thijs Schalk</div>
+
+              <div class="w-1/3">City:</div>
+              <div class="w-2/3">Leerdam</div>
+
+              <div class="w-1/3">Country:</div>
+              <div class="w-2/3">The Netherlands</div>
+
+              <div class="w-1/3">E-mail</div>
+              <div class="w-2/3">thijs.schalk@gmail.com</div>
+            </div>
+          </div>
+        </section>
+
+        <section class="flex flex-col border-b-2 lg:flex-row lg:border-b-0 mb-3 pb-3">
+          <div class="flex w-full lg:w-1/4 mb-2">
+            <div class="w-full h-full lg:border-r-2 mr-4">
+              <div class="font-semibold text-2xl text-gray-700 uppercase">About me</div>
+            </div>
+          </div>
+          <div class="w-full lg:w-3/4">
+            <p class="mb-4">
+              I'm a down to earth, friendly lad that loves software development. I deliver clean code which is maintainable and efficient. With my independent
+              and rational thinking there's a solution to every problem. Both as a solo developer and in a team I'm able to realize great projects. The web has
+              been a passion of mine and motivates me immensely. Especially using technology to optimize processes and help people be more productive motivates
+              me.
+            </p>
+            <p>My hobby's are:</p>
+            <ul class="list-disc pl-4 mb-4">
+              <li>Software programming</li>
+              <li>Films/Series/YouTube</li>
+              <li>
+                Video games (<a href="https://store.steampowered.com/app/252950/Rocket_League/" target="_blank" class="text-link">Rocket League</a>,
+                <a href="https://www.ea.com/games/battlefield" target="_blank" class="text-link">Battlefield</a>)
+              </li>
+              <li>Fitness (<a href="https://stronglifts.com" target="_blank" class="text-link">Stronglifts</a>)</li>
+            </ul>
+
+            <p>My inspirations are:</p>
+            <ul class="list-disc pl-4">
+              <li><a href="https://www.youtube.com/user/caseyneistat" target="_blank" class="text-link">Casey Neistat</a></li>
+              <li><a href="https://en.wikipedia.org/wiki/Steve_Jobs" target="_blank" class="text-link">Former Apple CEO Steve Jobs</a></li>
+              <li><a href="https://opensource.org/" target="_blank" class="text-link">The concept of Open Source</a></li>
+              <li><a href="https://michaelhyatt.com/" target="_blank" class="text-link">Entrepreneur Michael Hyatt</a></li>
+              <li>
+                Movies like <a href="https://www.imdb.com/title/tt1285016/" target="_blank">The Social Network</a>,
+                <a href="https://www.imdb.com/title/tt1375666/" target="_blank" class="text-link">Inception</a>,
+                <a href="https://www.imdb.com/title/tt0168122/" target="_blank" class="text-link">Pirates of Silicon Valley</a> and
+                <a href="https://www.imdb.com/title/tt0113243" target="_blank" class="text-link">Hackers</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section class="flex flex-col border-b-2 lg:flex-row lg:border-b-0 mb-3 pb-3">
+          <div class="flex w-full lg:w-1/4 mb-2">
+            <div class="w-full h-full lg:border-r-2 mr-4">
+              <div class="font-semibold text-2xl text-gray-700 uppercase">Education</div>
+            </div>
+          </div>
+          <div class="w-full lg:w-3/4">
+            <table class="table-auto w-full text-left whitespace-no-wrap">
+              <tbody>
+                <tr class="bg-gray-100">
+                  <td class="px-4 py-3">2002 - 2006</td>
+                  <td class="px-4 py-3">
+                    VMBO Theoretical<br />
+                    <a href="https://www.hetheerenlanden.nl" title="Heerenlanden College in Leerdam" target="_blank" class="text-link">Heerenlanden College</a>
+                  </td>
+                  <td class="px-4 py-3">Graduated</td>
+                </tr>
+                <tr class="bg-gray-50">
+                  <td class="border-t-2 border-gray-200 px-4 py-3">2006 - 2010</td>
+                  <td class="border-t-2 border-gray-200 px-4 py-3">
+                    MBO ICT Manager, Application Development Level 4<br />
+                    ICT Academie Utrecht
+                  </td>
+                  <td class="border-t-2 border-gray-200 px-4 py-3">Graduated</td>
+                </tr>
+                <tr class="bg-gray-100">
+                  <td class="border-t-2 border-gray-200 px-4 py-3">2010 - 2014</td>
+                  <td class="border-t-2 border-gray-200 px-4 py-3">
+                    HBO Informatica + Minor Web &amp; Multimedia<br />
+                    Hogeschool Utrecht
+                  </td>
+                  <td class="border-t-2 border-gray-200 px-4 py-3">Graduated</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+
+        <section class="flex flex-col border-b-2 lg:flex-row lg:border-b-0 mb-3 pb-3">
+          <div class="flex w-full lg:w-1/4 mb-2">
+            <div class="w-full h-full lg:border-r-2 mr-4">
+              <div class="font-semibold text-2xl text-gray-700 uppercase">Section title</div>
+            </div>
+          </div>
+          <div class="w-full lg:w-3/4">Section</div>
+        </section>
+      </div>
+    </section>
   </div>
 </template>
